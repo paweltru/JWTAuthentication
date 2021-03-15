bb79bdf4559e95dc2978831093fddb728c33d6ec
Paweł Trumiński <pawel.truminski@gmail.com>
2021-03-15 22:19:40 +0100

    Add project files.

---
+++ JWTAuthentication/Controllers/AuthenticateController.cs
@@ -0,0 +1,64 @@
+﻿using JWTAuthentication.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JWTAuthentication.Controllers
+{
+    [Route("identity")]
+    [ApiController]
+    public class AuthenticateController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private Dictionary<string, string> _users;
+
+        public AuthenticateController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _users = new Dictionary<string, string>
+            {
+                {"user", "password" }
+            };
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login([FromBody] LoginModel model)
+        {
+           // var user = await userManager.FindByNameAsync(model.Username);
+            if (_users.TryGetValue(model.Username, out var value) && value == model.Password)
+            {
+                var authClaims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, model.Username),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                };
+
+                var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"]));
+
+                var token = new JwtSecurityToken(
+                    issuer: _configuration["JWT:ValidIssuer"],
+                    audience: _configuration["JWT:ValidAudience"],
+                    expires: DateTime.Now.AddHours(3),
+                    claims: authClaims,
+                    signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
+                    );
+
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token),
+                    expiration = token.ValidTo
+                });
+            }
+            return Unauthorized();
+        }
+    }
+}
---
+++ JWTAuthentication/Filters/ApiKeyAttribute.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JWTAuthentication.Filters
+{
+    [AttributeUsage(validOn: AttributeTargets.Class | AttributeTargets.Method)]
+    public class ApiKeyAttribute : Attribute, IAsyncActionFilter
+    {
+        private const string ApiKeyName = "X-APIKEY";
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var routeVals = context.ActionDescriptor.RouteValues;
+
+            if (routeVals.ContainsKey("controller") && routeVals["controller"].Equals("Identity"))
+            { }
+            else
+            {
+                if (context.HttpContext.User.Identity.IsAuthenticated) { }
+                else
+                {
+                    if (!context.HttpContext.Request.Headers.TryGetValue(ApiKeyName, out var extractedApiKey))
+                    {
+                        context.Result = new ContentResult()
+                        {
+                            StatusCode = 401,
+                            Content = "Api Key was not provided"
+                        };
+                        return;
+                    }
+
+                    var appSettings = context.HttpContext.RequestServices.GetRequiredService<IConfiguration>();
+
+                    var apiKey = appSettings.GetValue<string>(ApiKeyName);
+
+                    if (!apiKey.Equals(extractedApiKey))
+                    {
+                        context.Result = new ContentResult()
+                        {
+                            StatusCode = 401,
+                            Content = "Api Key is not valid"
+                        };
+                        return;
+                    }
+                }
+
+            }
+
+
+            await next();
+        }
+    }
+}
